453676f79d0471c499812c58c3270f5de022f07d
李应 <2439175930@qq.com>
2022-11-26 16:11:13 +0800

    处理option 请求

--- src/main/java/com/evan/wj/utils/ControllerInterceptor.java
+++ src/main/java/com/evan/wj/utils/ControllerInterceptor.java
@@ -1,11 +1,16 @@
 package com.evan.wj.utils;
 
 import com.alibaba.fastjson.JSONObject;
+import io.netty.handler.codec.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,31 +28,47 @@ public class ControllerInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         String token = request.getHeader("Authentication");
         String origin = request.getHeader("Origin");
-        if (token != null) {
+
+        // 如果是OPTIONS则结束请求
+        if (HttpMethod.OPTIONS.toString().equals(request.getMethod())) {
+            response.setHeader("Access-Control-Allow-Origin", request.getHeader("origin"));
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS");
+            response.setHeader("Access-Control-Max-Age", "86400");
+            response.setHeader("Access-Control-Allow-Headers", "Authentication");
+            response.setStatus(HttpStatus.NO_CONTENT.value());
+            return false;
+        }
+
+        if (!token.equals("undefined")) {
             Map<String, String> tokenVerify = JWTUtils.handlerJWTVerifier(token);
             String isToken = tokenVerify.get("isToken");
             if (!Boolean.parseBoolean(isToken)) {
-                try {
-                    Map<String, String> data = new HashMap<>();
-                    data.put("errorMsg", tokenVerify.get("errorMsg"));
-                    data.put("code", "401");
-                    response.addHeader("Access-Control-Allow-Credentials", "true");
-                    response.addHeader("Access-Control-Allow-Origin", origin);
-                    response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-                    response.addHeader("Access-Control-Allow-Headers", "Content-Type,X-CAF-Authorization-Token,sessionToken,X-TOKEN");
-                    response.setContentType("application/json; charset=utf-8");
-                    response.setStatus(401);
-                    PrintWriter writer = response.getWriter();
-                    writer.print(JSONObject.toJSONString(data));
-                    writer.close();
-                    return false;
-                } catch (Exception exception) {
-                    System.out.println(exception);
-                }
-
+                return result(response, origin);
             }
             return true;
         }
-        return true;
+        return result(response, origin);
+    }
+
+    private boolean result(HttpServletResponse response, String origin) {
+        try {
+        Map<String, String> data = new HashMap<>();
+        data.put("errorMsg", "当前用户未登录");
+        data.put("code", "401");
+        response.addHeader("Access-Control-Allow-Credentials", "true");
+        response.addHeader("Access-Control-Allow-Origin", origin);
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+        response.addHeader("Access-Control-Allow-Headers", "Content-Type,X-CAF-Authorization-Token,sessionToken,X-TOKEN");
+        response.setContentType("application/json; charset=utf-8");
+        response.setStatus(401);
+        PrintWriter writer = null;
+        writer = response.getWriter();
+        writer.print(JSONObject.toJSONString(data));
+        writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
