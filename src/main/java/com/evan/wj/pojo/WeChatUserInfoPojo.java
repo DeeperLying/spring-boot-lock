3202c4006fbb145d0ff45afea2711b34c03ff414
李应 <2439175930@qq.com>
2023-03-12 10:16:15 +0800

    feat: open magic mysql door 不再是增删改查

--- src/main/java/com/evan/wj/pojo/WeChatUserInfoPojo.java
+++ src/main/java/com/evan/wj/pojo/WeChatUserInfoPojo.java
@@ -11,30 +11,6 @@ import java.util.List;
  */
 @Data
 public class WeChatUserInfoPojo {
-    public WeChatUserInfoPojo() {}
-
-    public WeChatUserInfoPojo(
-            String openid,
-            String nickname,
-            int sex,
-            String language,
-            String city,
-            String province,
-            String country,
-            String headimgurl,
-            List<Object> privilege,
-            String unionid) {
-        this.openid =openid;
-        this.nickname = nickname;
-        this.sex =sex;
-        this.language = language;
-        this.city =city;
-        this.province = province;
-        this.country = country;
-        this.headimgurl =headimgurl;
-        this.privilege = privilege;
-        this.unionid = unionid;
-    }
 
     String openid;
 
@@ -55,4 +31,8 @@ public class WeChatUserInfoPojo {
     List<Object> privilege;
 
     String unionid;
+
+    long errcode;
+
+    String errmsg;
 }
