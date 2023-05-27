85c62067c4c7e5d1f27d294e2261e6455ec160be
李应 <2439175930@qq.com>
2023-05-28 02:16:28 +0800

    feat: init rabbitMQ

---
+++ src/main/java/com/evan/wj/utils/RabbitMQConfig.java
@@ -0,0 +1,8 @@
+package com.evan.wj.utils;
+
+/**
+ * @author SuperLee
+ * @date 2023/5/28 上午12:15
+ */
+public class RabbitMQConfig {
+}
---
+++ src/main/java/com/evan/wj/utils/RabbitMQConsumer.java
@@ -0,0 +1,8 @@
+package com.evan.wj.utils;
+
+/**
+ * @author SuperLee
+ * @date 2023/5/28 上午12:53
+ */
+public class RabbitMQConsumer {
+}
